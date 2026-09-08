b7e0a7672e75c2000916930ae6b6370095c204eb
MaXmEd <daniilussuslov@gmail.com>
2026-09-08 11:36:15 +0300

    debug: isolate prisma query

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -4,40 +4,51 @@ import { PrismaService } from '../prisma/prisma.service.js';
 @Injectable()
 export class ProfileService {
   constructor(private readonly prismaService: PrismaService) {}
+  // async getProfile() {
+  //   const profile = await this.prismaService.profile.findFirst({
+  //     select: {
+  //       name: true,
+  //       description: true,
+  //       github: true,
+  //       skills: {
+  //         select: {
+  //           name: true,
+  //           level: true,
+  //         },
+  //       },
+  //       experience: {
+  //         select: {
+  //           company: true,
+  //           position: true,
+  //           startDate: true,
+  //           endDate: true,
+  //           achievements: true,
+  //         },
+  //       },
+  //       projects: {
+  //         select: {
+  //           name: true,
+  //           url: true,
+  //         },
+  //       },
+  //     },
+  //   });
+
+  //   if (!profile) {
+  //     throw new NotFoundException();
+  //   }
+
+  //   return profile;
+  // }
+
   async getProfile() {
-    const profile = await this.prismaService.profile.findFirst({
-      select: {
-        name: true,
-        description: true,
-        github: true,
-        skills: {
-          select: {
-            name: true,
-            level: true,
-          },
-        },
-        experience: {
-          select: {
-            company: true,
-            position: true,
-            startDate: true,
-            endDate: true,
-            achievements: true,
-          },
-        },
-        projects: {
-          select: {
-            name: true,
-            url: true,
-          },
-        },
-      },
-    });
-
-    if (!profile) {
-      throw new NotFoundException();
-    }
-
-    return profile;
-  }
+  return {
+    name: 'Test',
+    description: 'Test',
+    github: 'https://github.com/test',
+    skills: [],
+    experience: [],
+    projects: [],
+  };
+}
 }
