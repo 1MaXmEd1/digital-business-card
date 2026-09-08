5be221bd555726b339de4e6c93f877675a071b69
MaXmEd <daniilussuslov@gmail.com>
2026-09-08 13:45:32 +0300

    debug: disable database initializer

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-  console.log('=== 1: bootstrap started ===');
-
   const app = await NestFactory.create(AppModule);
-
-  console.log('=== 2: NestFactory.create completed ===');
-
   await app.listen(process.env.PORT ?? 3000);
-
-  console.log('=== 3: app.listen completed ===');
 }
-
 await bootstrap();
