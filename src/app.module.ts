b2f368de227e59928ba7ecfe71ce85d063e94941
MaXmEd <daniilussuslov@gmail.com>
2026-09-09 18:49:28 +0300

    debug: add apollo interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseInitializerModule } from './database/database-initializer.module.js';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
-      csrfPrevention: false, 
+      csrfPrevention: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()], 
     }),
     ConfigModule.forRoot({
       isGlobal: true,
