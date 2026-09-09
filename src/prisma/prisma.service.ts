c56e14741e3cffa8a02cdd1fdfd4f07c43e7b573
MaXmEd <daniilussuslov@gmail.com>
2026-09-09 09:12:07 +0300

    debug: use environment variable for prisma connection

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,23 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor(private readonly configService: ConfigService) {
-    const databaseUrl = configService.get('DATABASE_URL');
+  constructor() {
+    const databaseUrl = process.env.DATABASE_URL;
 
     if (!databaseUrl) {
       throw new Error('DATABASE_URL is not defined');
     }
 
-    const pool = new Pool({
-      connectionString: databaseUrl,
-    });
-
-    const adapter = new PrismaPg(pool);
+    const adapter = new PrismaPg({ connectionString: databaseUrl});
 
     super({ adapter });
   }
